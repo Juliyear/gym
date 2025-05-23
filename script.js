a3d7eb08a22f0b3f720cceba927e330ff3f8ae7d
Juliano Paulo <39599156+Juliyear@users.noreply.github.com>
2025-05-23 16:21:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -51,12 +51,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Os nomes das chaves (Nome, GrupoMuscular, etc.) devem corresponder EXATAMENTE
         // aos cabeçalhos da sua planilha Google
-        const payload = {
-            "Nome": exerciseName,
-            "GrupoMuscular": groupMuscular,
-            "SubGrupoMuscular": focus,
-            "LinkImagem": imageLink
-        };
+        curl -X POST "https://api.sheety.co/d528f68442f942c04387dec50427dfac/gym2/exercicios" \
+-H "Content-Type: application/json" \
+-d '{
+  "exercicio": {
+    "Nome": "Teste",
+    "GrupoMuscular": "Costas",
+    "SubGrupoMuscular": "Dorsal",
+    "LinkImagem": "https://exemplo.com/img.gif"
+  }
+}'
 
         const result = await postDataToSheety(payload);
 
